36462f28ab4a80350c92f3d71af1f630e1786e66
vijaykrishna483-cms <vijay762005@gmail..com>
2025-06-25 19:45:15 +0530

    DFSDF

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app=express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://smartcrmtest.netlify.app']
 }));
 
 
