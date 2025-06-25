ed0d1eb22fbf0f73a76c5ce6048aa94117f88002
vijaykrishna483-cms <vijay762005@gmail..com>
2025-06-25 19:40:04 +0530

    DFDSF

--- frontend/src/libs/apiCall.js
+++ frontend/src/libs/apiCall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // NOT "localhost:5000/api"
+  baseURL: 'https://crm-7o4h.onrender.com/api', // NOT "localhost:5000/api"
 });
 // export function setAuthToken(token) {
 //   if (token) {
